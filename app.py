d5fb72b1a92eb0a64e7ab2b0118ea8179fbdc491
muthinaabhilash-creator <muthinaabhilash@gmail.com>
2026-06-18 12:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ st.title("Zyro Dynamics HR Help Desk")
 question = st.text_input("Ask an HR Question")
 
 if question:
+    response = ask_bot(question)
     st.write("HR Bot Response")
+    st.write(response["answer"])
