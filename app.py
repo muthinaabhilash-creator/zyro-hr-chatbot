873fa41f34b4051f47c94fbd6ed26e65f09910e4
muthinaabhilash-creator <muthinaabhilash@gmail.com>
2026-06-18 12:35:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("Zyro Dynamics HR Help Desk")
 question = st.text_input("Ask an HR Question")
 
 if question:
-st.subheader("HR Bot Response")
-st.write("Demo chatbot deployed successfully.")
+    st.subheader("HR Bot Response")
+    st.write("Demo chatbot deployed successfully.")
